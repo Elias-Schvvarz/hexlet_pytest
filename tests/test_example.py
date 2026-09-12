f0eb82d6b4972973f4c5f47d9e2025520aef239f
elias <eliaschvvarz@gmail.com>
2026-09-12 20:13:51 +0300

    advance test for reverse

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,16 @@
+from pathlib import Path
 from hexlet_pytest.example import reverse
 
-def test_reverse():
-    assert reverse('Hexlet') == 'telxeH'
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
 
-def test_reverse_for_empty_string():
-    assert reverse('') == ''
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+def test_example():
+    before_example = read_file('before_example')
+    expected = read_file('after_example')
+    actual = reverse(before_example)
+
+    assert actual == expected
